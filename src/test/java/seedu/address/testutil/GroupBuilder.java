ac6648c78af461aab48dfd9d90f48bc3f4cfc3fb
xinyee20 <codelearner19@gmail.com>
2020-09-30 16:54:09 +0800

    Fix import issues and Checkstyle

--- src/test/java/seedu/address/testutil/GroupBuilder.java
+++ src/test/java/seedu/address/testutil/GroupBuilder.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.commons.utils.CsvUtil;
+import seedu.address.commons.util.CsvUtil;
 import seedu.address.model.group.Group;
 import seedu.address.model.group.Lesson;
-import seedu.address.model.group.StudentInfo;
 import seedu.address.model.group.Student;
+import seedu.address.model.group.StudentInfo;
 import seedu.address.model.util.SampleDataUtil;
 
 /**
