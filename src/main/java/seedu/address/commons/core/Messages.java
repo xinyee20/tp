3e1590c18f0b4f97524a9801f28340cd659fee10
ryanlimjr <ryanlimjr@gmail.com>
2020-10-02 19:10:35 +0800

    Modify Messages.java to display approprate result messages.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,4 +10,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
+    //serenity messages
+
+    public static final String MESSAGE_GROUP_LISTED_OVERVIEW = "group %1$s is listed!";
+    public static final String MESSAGE_GROUP_EMPTY = "no such group!";
 }
--- src/main/java/seedu/address/logic/commands/ViewGrpCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewGrpCommand.java
@@ -1,13 +1,36 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
+import seedu.address.model.group.GrpContainsKeywordPredicate;
 
 public class ViewGrpCommand extends Command {
 
     public static final String COMMAND_WORD = "viewgrp";
+    public static final Object MESSAGE_USAGE = COMMAND_WORD
+            + ": Finds all students who are part of "
+            + "the specified group and displays them as a list with index numbers.\n"
+            + "Parameters: GROUP \n"
+            + "Example: " + COMMAND_WORD + " G04";
+
+    private final GrpContainsKeywordPredicate predicate;
+
+    public ViewGrpCommand(GrpContainsKeywordPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    private String getMessage(Model model) {
+        return model.getFilteredGroupList().isEmpty() ?
+                Messages.MESSAGE_GROUP_EMPTY
+                : String.format(Messages.MESSAGE_GROUP_LISTED_OVERVIEW, model.getFilteredGroupList().get(0).getName());
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(COMMAND_WORD);
+        requireNonNull(model);
+        model.updateFilteredGroupList(predicate);
+        return new CommandResult(this.getMessage(model));
     }
 }
