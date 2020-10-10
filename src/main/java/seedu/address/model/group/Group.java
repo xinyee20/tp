3f13452ac054d97987c4279c56cffa94a270c41a
successs404 <neoruien@u.nus.edu>
2020-10-10 22:44:46 +0800

    Fix minor errors

--- src/main/java/seedu/address/model/group/Group.java
+++ src/main/java/seedu/address/model/group/Group.java
@@ -64,14 +64,14 @@ public class Group {
      *
      * @param name     A valid name.
      * @param students A list of students.
-     * @param lessons  A list of tutorial classes.
+     * @param lessons  A list of tutorial lessons.
      */
 
-    public Group(String name, UniqueStudentList students, UniqueLessonList classes) {
-        requireAllNonNull(name, students, classes);
+    public Group(String name, UniqueStudentList students, UniqueLessonList lessons) {
+        requireAllNonNull(name, students, lessons);
         this.name = name;
         this.students = students;
-        this.lessons = lessons;
+        this.lessons = this.lessons;
     }
 
     public String getName() {
