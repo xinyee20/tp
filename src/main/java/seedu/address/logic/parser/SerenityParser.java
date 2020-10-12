10b3ade0ebc37455264f867212e0e91d6bbf4ead
Cy <chunyonggg@gmail.com>
2020-10-12 17:35:15 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/SerenityParser.java
+++ src/main/java/seedu/address/logic/parser/SerenityParser.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddGrpCommand;
-import seedu.address.logic.commands.AddStudentCommand;
 import seedu.address.logic.commands.AddQnCommand;
+import seedu.address.logic.commands.AddStudentCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DelGrpCommand;
