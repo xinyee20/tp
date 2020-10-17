0d9e5c0e4bf098c03ac45b0821b5b368c9b6cad1
xinyee20 <codelearner19@gmail.com>
2020-10-18 03:59:31 +0800

    Remove Comments

--- src/test/java/seedu/address/logic/parser/MarkPresentCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkPresentCommandParserTest.java
@@ -1,19 +1,11 @@
 package seedu.address.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.MarkPresentCommand;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.group.StudentInfo;
 
 class MarkPresentCommandParserTest {
 
@@ -22,48 +14,15 @@ class MarkPresentCommandParserTest {
     @Test
     public void execute_missingStudentName_throwsParseException() {
 
-//        String arguments = "markpresent id/e0123456";
-//
-//        MarkPresentCommandParser markPresentCommandParser = new MarkAbsentCommandParser().parse(arguments);
-//
-//        String expectedMessage = String.format(MarkPresentCommandParser.MESSAGE_STUDENT_NOT_GIVEN);
-
-
-        /**
-         * Enter lesson
-         * Get UniqueStudentInfo List
-         * Missing student id prefix
-         * Throw Exception
-         */
     }
 
     @Test
     public void execute_missingStudentId_throwsCommandException() {
 
-//        String arguments = "markpresent name/Aaron Tan";
-//
-//        MarkPresentCommandParser markPresentCommandParser = new MarkAbsentCommandParser().parse(arguments);
-//
-//        String expectedMessage = String.format(MarkPresentCommandParser.MESSAGE_STUDENT_NOT_GIVEN);
-
-
-        /**
-         * Enter lesson
-         * Get UniqueStudentInfo List
-         * Missing student id prefix
-         * Throw Exception
-         */
     }
 
     @Test
     public void execute_invalidParameter_throwsCommandException() {
-
-        /**
-         * Enter lesson
-         * Get UniqueStudentInfo List
-         * Does not understand parameter, it is not 'all'
-         * Throw Exception
-         */
 
     }
 
