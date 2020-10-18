c6a44c95f137bc4a84e972eae7f6f4c1ce229625
Ryan Lim <22640268+ryanlimjr@users.noreply.github.com>
2020-10-18 20:44:00 +0800

    Remove redundant UI components and greated a general data display UI component for futrue data display screens

--- src/main/java/seedu/address/ui/groupdata/StudentCard.java
+++ src/main/java/seedu/address/ui/groupdata/StudentCard.java
@@ -1,10 +1,11 @@
-package seedu.address.ui;
+package seedu.address.ui.groupdata;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.group.Student;
+import seedu.address.ui.UiPart;
 
 
 /**
