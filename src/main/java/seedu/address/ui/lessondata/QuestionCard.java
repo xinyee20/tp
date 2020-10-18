a4aac5c650e71e8ffb34e51cd2d428a137b917a3
Ryan Lim <22640268+ryanlimjr@users.noreply.github.com>
2020-10-19 00:10:00 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/ui/lessondata/QuestionCard.java
+++ src/main/java/seedu/address/ui/lessondata/QuestionCard.java
@@ -2,14 +2,10 @@ package seedu.address.ui.lessondata;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.group.Question;
-import seedu.address.model.person.Person;
 import seedu.address.ui.UiPart;
-
-import java.util.Comparator;
 
 /**
  * An UI component that displays information of a {@code Person}.
