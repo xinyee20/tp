588c7ddef5f15b0c5110f62c8af29e86dbc36b3b
xinyee20 <codelearner19@gmail.com>
2020-10-24 14:31:51 +0800

    Add indexing for delstudent command

--- src/main/java/team/serenity/logic/commands/DelStudentCommand.java
+++ src/main/java/team/serenity/logic/commands/DelStudentCommand.java
@@ -10,29 +10,39 @@ import static team.serenity.logic.parser.CliSyntax.PREFIX_NAME;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
+import team.serenity.commons.core.index.Index;
 import team.serenity.logic.commands.exceptions.CommandException;
 import team.serenity.model.Model;
 import team.serenity.model.group.Group;
 import team.serenity.model.group.Student;
+import team.serenity.model.group.UniqueStudentList;
+import team.serenity.model.util.UniqueList;
 
 public class DelStudentCommand extends Command {
 
     public static final String COMMAND_WORD = "delstudent";
+    public static final String MESSAGE_SUCCESS = "You removed %s (%s) from tutorial group %s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
+            "Index %d is not found, please ensure that it exists";
+
     public static final String MESSAGE_USAGE = COMMAND_WORD
         + ": Removes a new Student from a specified tutorial group. \n"
         + "Parameters: "
         + PREFIX_GRP + "GROUP "
         + PREFIX_NAME + "STUDENT_NAME "
-        + PREFIX_ID + "STUDENT_ID\n"
+        + PREFIX_ID + "STUDENT_NUMBER " + "or INDEX\n"
         + "Example: " + COMMAND_WORD + " "
         + PREFIX_GRP + "G04 "
         + PREFIX_NAME + "Ryan "
-        + PREFIX_ID + "e1234567\n";
+        + PREFIX_ID + "e1234567\n"
+        + "or " + COMMAND_WORD + " 2 "
+            + PREFIX_GRP + "G04";
 
-    public static final String MESSAGE_SUCCESS = "You removed %s (%s) from tutorial group %s";
-
-    private final String studentName;
-    private final String studentId;
+    private String studentName;
+    private String studentId;
+    private Student toDelete;
+    private Index index;
+    private boolean isByIndex;
     private final Predicate<Group> predicate;
 
     /**
@@ -46,28 +56,50 @@ public class DelStudentCommand extends Command {
         this.studentName = studentName;
         this.studentId = studentId;
         this.predicate = predicate;
+        this.isByIndex = false;
+    }
+
+    /**
+     * Creates a DelStudentCommand to remove the specified {@code Student} by index.
+     * @param index Index of student in the list
+     * @param predicate Group predicate
+     */
+    public DelStudentCommand(Index index, Predicate<Group> predicate) {
+        requireAllNonNull(index, predicate);
+        this.index = index;
+        this.predicate = predicate;
+        this.isByIndex = true;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        Student student = new Student(this.studentName, this.studentId);
         model.updateFilteredGroupList(this.predicate);
         ObservableList<Group> groups = model.getFilteredGroupList();
+        UniqueList<Student> uniqueStudentList = groups.get(0).getStudents();
 
         if (groups.isEmpty()) {
             //no such group
             throw new CommandException(MESSAGE_GROUP_EMPTY);
         }
 
-        if (!groups.get(0).getStudents().contains(student)) {
-            //student does not exist
-            throw new CommandException(MESSAGE_STUDENT_EMPTY);
+        if (!isByIndex) {
+            toDelete = new Student(this.studentName, this.studentId);
+            if (! uniqueStudentList.contains(toDelete)) {
+                //student does not exist
+                throw new CommandException(MESSAGE_STUDENT_EMPTY);
+            }
+        } else {
+            if (index.getZeroBased() > uniqueStudentList.size()) {
+                throw new CommandException(String.format(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, index.getOneBased()));
+            }
+
+            toDelete = uniqueStudentList.getList().get(index.getZeroBased());
         }
 
-        model.deleteStudentFromGroup(student, this.predicate);
+        model.deleteStudentFromGroup(toDelete, this.predicate);
         model.updateFilteredGroupList(this.predicate);
         return new CommandResult(
-            String.format(MESSAGE_SUCCESS, this.studentName, this.studentId,
+            String.format(MESSAGE_SUCCESS, toDelete.getName(), toDelete.getStudentId(),
             model.getFilteredGroupList().get(0).getName()));
     }
 
--- src/main/java/team/serenity/logic/parser/DelStudentCommandParser.java
+++ src/main/java/team/serenity/logic/parser/DelStudentCommandParser.java
@@ -7,6 +7,7 @@ import static team.serenity.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.stream.Stream;
 
+import team.serenity.commons.core.index.Index;
 import team.serenity.logic.commands.DelStudentCommand;
 import team.serenity.logic.parser.exceptions.ParseException;
 import team.serenity.model.group.GroupContainsKeywordPredicate;
@@ -23,28 +24,42 @@ public class DelStudentCommandParser implements Parser<DelStudentCommand> {
     public DelStudentCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_GRP, PREFIX_NAME, PREFIX_ID);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_GRP, PREFIX_NAME, PREFIX_ID)
-            || !argMultimap.getPreamble().isEmpty()) {
+        if (!arePrefixesPresent(argMultimap, PREFIX_GRP)) {
             throw this.deleteStudentCommandParserException;
         }
 
+        Index index;
         String[] grpKeywordArray = argMultimap.getValue(PREFIX_GRP).get().split("\\s+");
-        String[] studentNameArray = argMultimap.getValue(PREFIX_NAME).get().split("\\s+");
-        String[] studentIdArray = argMultimap.getValue(PREFIX_ID).get().split("\\s+");
 
-        //if id or group keyword is more than 1, or if student name has more than 10 letters, throw exception
-        boolean matchesGrp = grpKeywordArray.length == 1;
-        boolean matchesId = studentIdArray.length == 1 && studentIdArray[0].length() == 8;
-        boolean matchesStudentName = studentNameArray.length <= 10;
-        if (!matchesGrp || !matchesId || !matchesStudentName) {
-            throw this.deleteStudentCommandParserException;
+        if (argMultimap.getValue(PREFIX_NAME).isPresent() && argMultimap.getValue(PREFIX_ID).isPresent()) {
+            String[] studentNameArray = argMultimap.getValue(PREFIX_NAME).get().split("\\s+");
+            String[] studentIdArray = argMultimap.getValue(PREFIX_ID).get().split("\\s+");
+
+            //if id or group keyword is more than 1, or if student name has more than 10 letters, throw exception
+            boolean matchesGrp = grpKeywordArray.length == 1;
+            boolean matchesId = studentIdArray.length == 1 && studentIdArray[0].length() == 8;
+            boolean matchesStudentName = studentNameArray.length <= 10;
+            if (! matchesGrp || ! matchesId || ! matchesStudentName) {
+                throw this.deleteStudentCommandParserException;
+            }
+
+            String studentName = String.join(" ", studentNameArray);
+            String studentId = studentIdArray[0];
+            String grpName = grpKeywordArray[0];
+
+            return new DelStudentCommand(studentName, studentId, new GroupContainsKeywordPredicate(grpName));
+
+        } else {
+            index = SerenityParserUtil.parseIndex(argMultimap.getPreamble());
+            String grpName = grpKeywordArray[0];
+
+            boolean matchesGrp = grpKeywordArray.length == 1;
+            if (!matchesGrp) {
+                throw this.deleteStudentCommandParserException;
+            }
+
+            return new DelStudentCommand(index, new GroupContainsKeywordPredicate(grpName));
         }
-
-        String studentName = String.join(" ", studentNameArray);
-        String studentId = studentIdArray[0];
-        String grpName = grpKeywordArray[0];
-
-        return new DelStudentCommand(studentName, studentId, new GroupContainsKeywordPredicate(grpName));
     }
 
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
