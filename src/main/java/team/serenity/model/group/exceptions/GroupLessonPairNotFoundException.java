86f01037ebcfb9b4a9c242991a925cc75fb915ed
ryanlimjr <ryanlimjr@yahoo.com.sg>
2020-10-25 22:43:19 +0800

    Add GroupLessonPairNotFoundException to support StudentInfoManager operations

--- src/main/java/team/serenity/model/group/exceptions/GroupLessonPairNotFoundException.java
+++ src/main/java/team/serenity/model/group/exceptions/GroupLessonPairNotFoundException.java
@@ -1,2 +1,7 @@
-package team.serenity.model.group.exceptions;public class GroupLessonPairNotFound {
+package team.serenity.model.group.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified Group & Lesson Pairing.
+ */
+public class GroupLessonPairNotFoundException extends RuntimeException {
 }
