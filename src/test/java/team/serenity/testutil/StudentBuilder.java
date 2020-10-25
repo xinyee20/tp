6b45b6bd4d7d6a138e60bc82c382d94536ea742e
ryanlimjr <ryanlimjr@yahoo.com.sg>
2020-10-25 20:28:18 +0800

    Fix student id format

---
+++ src/test/java/team/serenity/testutil/StudentBuilder.java
@@ -0,0 +1,2 @@
+package team.serenity.testutil;public class StudentBuilder {
+}
--- src/test/java/team/serenity/testutil/TypicalGroups.java
+++ src/test/java/team/serenity/testutil/TypicalGroups.java
@@ -16,24 +16,24 @@ public class TypicalGroups {
 
     public static final Group GROUP_C = new GroupBuilder().withName("G06")
         .withStudents(
-            new Student("Jeffery", "e0000000"),
-            new Student("Luna", "e0111111"),
-            new Student("Queenie", "e0222222")
+            new Student("Jeffery", "A0000000U"),
+            new Student("Luna", "A0111111U"),
+            new Student("Queenie", "A0222222U")
         ).withClasses("4-2", "5-1")
         .build();
 
     public static final Group GROUP_D = new GroupBuilder().withName("G07")
         .withStudents(
-            new Student("Freddie", "e0000000"),
-            new Student("June", "e0101011")
+            new Student("Freddie", "A0000001U"),
+            new Student("June", "A0101011U")
         ).withClasses("4-2", "5-1", "5-2", "6-1")
         .build();
 
     // Manually added - Group's details found in {@code CommandTestUtil}
     public static final Group GROUP_A = new GroupBuilder().withName(CommandTestUtil.VALID_GRP_GROUP_A)
         .withFilePath(CommandTestUtil.VALID_PATH_GROUP_A).build();
-    public static final Group GROUP_B = new GroupBuilder().withName(CommandTestUtil.VALID_GRP_GROUP_B)
-        .withFilePath(CommandTestUtil.VALID_PATH_GROUP_B).build();
+    //    public static final Group GROUP_B = new GroupBuilder().withName(CommandTestUtil.VALID_GRP_GROUP_B)
+    //        .withFilePath(CommandTestUtil.VALID_PATH_GROUP_B).build();
 
     private TypicalGroups() {
     } // prevents instantiation
@@ -50,6 +50,6 @@ public class TypicalGroups {
     }
 
     public static List<Group> getTypicalGroups() {
-        return new ArrayList<>(Arrays.asList(GROUP_A, GROUP_B, GROUP_C, GROUP_D));
+        return new ArrayList<>(Arrays.asList(GROUP_A, GROUP_C, GROUP_D));
     }
 }
