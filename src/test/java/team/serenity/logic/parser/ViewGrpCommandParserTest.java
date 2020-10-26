e1c523b71ec89c3157135b695bc5bfb482566f3c
ryanlimjr <ryanlimjr@gmail.com>
2020-10-26 17:46:17 +0800

    fix checkstyle (idk how this error slipped into this but its fixed)

--- src/test/java/team/serenity/logic/parser/ViewGrpCommandParserTest.java
+++ src/test/java/team/serenity/logic/parser/ViewGrpCommandParserTest.java
@@ -1,46 +1,5 @@
 package team.serenity.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static team.serenity.testutil.Assert.assertThrows;
-
-import org.junit.jupiter.api.Test;
-
-import team.serenity.logic.commands.ViewGrpCommand;
-import team.serenity.logic.parser.exceptions.ParseException;
-import team.serenity.model.group.GroupContainsKeywordPredicate;
-
 class ViewGrpCommandParserTest {
 
-    private final ViewGrpCommandParser parser = new ViewGrpCommandParser();
-
-    @Test
-    void parse_emptyString_throwsParseException() {
-        assertThrows(ParseException.class, () -> this.parser.parse(""));
-    }
-
-    @Test
-    void parse_whiteSpace_throwsParseException() {
-        assertThrows(ParseException.class, () -> this.parser.parse("  "));
-    }
-
-    @Test
-    void parse_noPrefixRandomStringInput_throwsParseException() {
-        assertThrows(ParseException.class, () -> this.parser.parse("12da"));
-    }
-
-    @Test
-    void parse_prefixWithoutParameters_throwsParseException() {
-        assertThrows(ParseException.class, () -> this.parser.parse("grp/"));
-    }
-
-    @Test
-    void parse_prefixWithWhiteSpaceOnly_throwsParseException() {
-        assertThrows(ParseException.class, () -> this.parser.parse("grp/   "));
-    }
-
-    @Test
-    void parse_prefixWithParameters() throws ParseException {
-        ViewGrpCommand expected = new ViewGrpCommand(new GroupContainsKeywordPredicate("g04"));
-        assertEquals(expected, parser.parse("grp/g04"));
-    }
 }
