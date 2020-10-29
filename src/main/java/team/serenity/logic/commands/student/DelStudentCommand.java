de7f42d301849d237f716b2c152de9b3f4a9931f
xinyee20 <codelearner19@gmail.com>
2020-10-29 23:01:42 +0800

    Fix error message

--- src/main/java/team/serenity/logic/commands/student/DelStudentCommand.java
+++ src/main/java/team/serenity/logic/commands/student/DelStudentCommand.java
@@ -109,7 +109,7 @@ public class DelStudentCommand extends Command {
         model.deleteStudentFromGroup(toDelete.get(), this.predicate);
         model.updateFilteredGroupList(this.predicate);
         return new CommandResult(
-            String.format(MESSAGE_SUCCESS, this.studentName, this.studentId,
+            String.format(MESSAGE_SUCCESS, this.studentName.get(), this.studentId.get(),
             model.getFilteredGroupList().get(0).getGroupName()));
     }
 
