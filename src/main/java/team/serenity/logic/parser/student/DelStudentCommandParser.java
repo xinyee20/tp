9c317278957554803e94fbbddc4c545092cf65b5
Cy <chunyonggg@gmail.com>
2020-11-07 23:26:16 +0800

    Prevent both matric and index used in delstudent

--- src/main/java/team/serenity/logic/parser/student/DelStudentCommandParser.java
+++ src/main/java/team/serenity/logic/parser/student/DelStudentCommandParser.java
@@ -42,6 +42,13 @@ public class DelStudentCommandParser implements Parser<DelStudentCommand> {
         if (!argMultimap.getValue(PREFIX_NAME).isPresent() && argMultimap.getValue(PREFIX_MATRIC).isPresent()) {
             throw this.deleteStudentCommandParserException;
         }
+        if (argMultimap.getValue(PREFIX_NAME).isPresent() && argMultimap.getPreamble().length() != 0) {
+            throw this.deleteStudentCommandParserException;
+        }
+
+        if (argMultimap.getValue(PREFIX_MATRIC).isPresent() && argMultimap.getPreamble().length() != 0) {
+            throw this.deleteStudentCommandParserException;
+        }
 
         Index index;
         String[] grpKeywordArray = argMultimap.getValue(PREFIX_GRP).get().split("\\s+");
