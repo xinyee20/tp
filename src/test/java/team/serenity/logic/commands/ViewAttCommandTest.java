5374f6d52dd72c815db2bdeb784b5aa806dbc083
successs404 <neoruien@u.nus.edu>
2020-11-07 11:44:06 +0800

    Add tests for ViewAttCommandTest and ViewScoreCommandTest

--- src/test/java/team/serenity/logic/commands/ViewAttCommandTest.java
+++ src/test/java/team/serenity/logic/commands/ViewAttCommandTest.java
@@ -1,10 +1,10 @@
 package team.serenity.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static team.serenity.commons.core.Messages.MESSAGE_ATTENDANCE_LISTED_OVERVIEW;
 import static team.serenity.commons.core.Messages.MESSAGE_GROUP_EMPTY;
-import static team.serenity.commons.core.Messages.MESSAGE_GROUP_LISTED_OVERVIEW;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
@@ -18,30 +18,32 @@ import team.serenity.model.group.lesson.Lesson;
 import team.serenity.testutil.GroupBuilder;
 import team.serenity.testutil.ModelStub;
 
-public class ViewGrpCommandTest {
+public class ViewAttCommandTest {
 
     @Test
     void execute_noGroup() {
-        ViewGrpCommand test = new ViewGrpCommand(new GroupContainsKeywordPredicate("G01"));
-        CommandResult actual = test.execute(new ModelStubWithNoGroup());
+        ModelStub modelStub = new ModelStubWithNoGroup();
+        ViewAttCommand viewAttCommand = new ViewAttCommand(new GroupContainsKeywordPredicate("G01"));
+
+        CommandResult actual = viewAttCommand.execute(modelStub);
         assertEquals(MESSAGE_GROUP_EMPTY, actual.getFeedbackToUser());
     }
 
     @Test
     void execute_containsGroup() {
-        ModelStubWithGroup modelStub = new ModelStubWithGroup();
-        ViewGrpCommand test = new ViewGrpCommand(new GroupContainsKeywordPredicate("G04"));
-        CommandResult actual = test.execute(modelStub);
+        ModelStub modelStub = new ModelStubWithGroup();
+        ViewAttCommand viewAttCommand = new ViewAttCommand(new GroupContainsKeywordPredicate("G04"));
+        CommandResult actual = viewAttCommand.execute(modelStub);
         assertEquals(
-                String.format(MESSAGE_GROUP_LISTED_OVERVIEW, modelStub.getFilteredGroupList().get(0).getGroupName()),
-                actual.getFeedbackToUser()
+            String.format(MESSAGE_ATTENDANCE_LISTED_OVERVIEW, modelStub.getFilteredGroupList().get(0).getGroupName()),
+            actual.getFeedbackToUser()
         );
     }
 
-    private class ModelStubWithGroup extends ModelStub {
+    private static class ModelStubWithGroup extends ModelStub {
         private ObservableList<Group> groupList =
-                FXCollections.observableList(Arrays.asList(new GroupBuilder().build()));
-        private FilteredList<Group> filteredGroupList = new FilteredList<Group>(groupList);
+            FXCollections.observableList(Collections.singletonList(new GroupBuilder().build()));
+        private FilteredList<Group> filteredGroupList = new FilteredList<>(groupList);
 
         @Override
         public void updateFilteredGroupList(Predicate<Group> predicate) {
@@ -54,14 +56,12 @@ public class ViewGrpCommandTest {
         }
 
         @Override
-        public void updateFilteredLessonList(Predicate<Lesson> predicate) {
-            return;
-        }
+        public void updateFilteredLessonList(Predicate<Lesson> predicate) {}
     }
 
-    private class ModelStubWithNoGroup extends ModelStub {
-        private ObservableList<Group> groupList = FXCollections.observableList(Arrays.asList());
-        private FilteredList<Group> filteredGroupList = new FilteredList<Group>(groupList);
+    private static class ModelStubWithNoGroup extends ModelStub {
+        private ObservableList<Group> groupList = FXCollections.observableList(Collections.emptyList());
+        private FilteredList<Group> filteredGroupList = new FilteredList<>(groupList);
 
         @Override
         public void updateFilteredGroupList(Predicate<Group> predicate) {
@@ -74,8 +74,7 @@ public class ViewGrpCommandTest {
         }
 
         @Override
-        public void updateFilteredLessonList(Predicate<Lesson> predicate) {
-            return;
-        }
+        public void updateFilteredLessonList(Predicate<Lesson> predicate) {}
     }
+
 }
